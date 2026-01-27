16f296904ddbdbec5c27c615ed6a6b8b27ac94aa
Rustizas <rpreikstaitis@gmail.com>
2026-01-27 12:38:12 +0200

    feat: added translation script for eng text

---
+++ scripts/1_1_translate_wikipedia.py
@@ -0,0 +1,24 @@
+from transformers import pipeline
+from transformers.pipelines.pt_utils import KeyDataset
+import torch
+import pandas as pd
+from datasets import Dataset
+
+
+df = pd.read_csv("/home/rustis/projektai/Res/lto/data/raw/wikipedia_lithuanian_5k.csv")
+hf_dataset = Dataset.from_pandas(df)
+
+translator = pipeline(
+    task="translation",
+    model="facebook/nllb-200-distilled-600M",
+    torch_dtype=torch.bfloat16,
+    device=0,
+    max_length=1024)
+
+translated = []
+
+for out in translator(KeyDataset(hf_dataset, "sentences"), src_lang="lit_Latn", tgt_lang="eng_Latn", batch_size=32, max_length=1024):
+    translated.append(out[0]["translation_text"])
+
+df["english"] = translated
+df.to_csv("/home/rustis/projektai/Res/lto/data/raw/wikipedia_lt_eng.csv", index=False)
