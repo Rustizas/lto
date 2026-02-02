cf0daf498f22e1ca05f650ca2c99e58bfeb3af5c
Rustizas <rpreikstaitis@gmail.com>
2026-02-02 11:12:08 +0200

    feat: script for calulating summary

---
+++ scripts/1_3_analyze_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy
+import tiktoken
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+df = pd.read_csv("/home/rustis/projektai/Res/lto/data/raw/token_counts_5k.csv")
+
+model_names = ["llama3", "qwen2", "mistral", "gpt4"]
+
+data = []
+
+for name in model_names:
+	data.append({
+		"avg_ratio": round(df[f"ratio_{name}"].mean(), 2), #Avg ratio of Lithuanian to English tokens
+		"std_dev_ratio": round(df[f"ratio_{name}"].std(), 2), #Standart deviation of the ratio
+		"avg_char_per_token_lt": round(df[f"avg_char_per_token_{name}_lt"].mean(), 2), #Avg symbols per token in Lithuanian
+		"min_ratio": round(df[f"ratio_{name}"].min(), 2), #Min ratio of Lithuanian to English tokens
+		"max_ratio": round(df[f"ratio_{name}"].max(), 2), #Max ratio of Lithuanian to English tokens
+	})
+
+results_df = pd.DataFrame(data, index=model_names)
+results_df.to_csv("/home/rustis/projektai/Res/lto/data/processed/summary_stats.csv")
